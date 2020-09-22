354ae4855fa7133232ae3ec03ca509ec94d102cc
wnstj2007 <201802096@o.cnu.ac.kr>
2020-09-21 20:13:58 -0700

    server.py 실습 파일 형태로 완성

--- week3/201802096_server.py
+++ week3/201802096_server.py
@@ -9,3 +9,10 @@ sock.listen()
 
 conn, addr = sock.accept()
 print('Connection address : ' + str(addr))
+while True:
+	data = conn.recv(1024)
+	if not data:
+		break
+	print('Connection address : ' + data.decode())
+	conn.send(data)
+conn.close()
