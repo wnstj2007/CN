ef6f78259d8d11009455f1009f0bdea1b22dfaec
wnstj2007 <201802096@o.cnu.ac.kr>
2020-09-21 22:01:20 -0700

    각 클라이언트마다 점수 계산

--- week3/201802096_server.py
+++ week3/201802096_server.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 ip = ''
 port = 5001
@@ -7,12 +8,39 @@ sock = socket.socket(socket.AF_INET, sock.SOCK_STREAM)
 sock.bind((ip, port))
 sock.listen()
 
-conn, addr = sock.accept()
-print('Connection address : ' + str(addr))
+clients = [{} for _ in range(3)]
+for i in range(3):
+	conn, addr = sock.accept()
+	print('Connection address : ' + str(addr))
+	clients[i]['conn'] = conn
+	
+for i in clients:
+	i['conn'].send('Okay... All players have gathered. Start the game.'.encode())
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	print('Connection address : ' + data.decode())
-	conn.send(data)
+	#난수 10개 생성
+	numbers = [ random.randrange(1,101) for _ in range(10) ]
+	for i in clients:
+		#숫자 선택
+		i['conn'].send('Please select 1 number from 1 to 10.')
+		i['conn'].send('Number : '.encode())
+		num = i['conn'].recv(1024)
+		i['num1'] = numbers[num+1]
+		i['num2'] = random.randrange(-1, 5)
+		data = 'You chose the number ' + str(numbers[num+1]) + '. Please wait.'
+		i['conn'].send(data.encode())
+		
+	for i in clients:
+		#연산자 선택
+		i['conn'].send('Do you want multiply or add...?'.encode())
+		i['conn'].send('multiply or add : '.encode())
+		oper = i['conn'].recv(1024).decode()
+		if oper == 'add':
+			i['score'] = i['num1']+i['num2']
+		elif oper == 'multiply':
+			i['score'] = i['num1']*i['num2']
+		i['conn'].send('Okay... please wait.'.encode())
+		
+		
+	for i in clients:
+		pass
 conn.close()
