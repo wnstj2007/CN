5038f57bc8b72ab398a9302a8218e0551369ace2
wnstj2007 <201802096@o.cnu.ac.kr>
2020-09-23 03:41:32 -0700

    최종 수정

--- week3/201802096_client.py
+++ week3/201802096_client.py
@@ -7,7 +7,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, port))
 
 
-print(sock.recv(1024).decode())
+print(sock.recv(50).decode())
 
 #숫자 입력
 print(sock.recv(1024).decode())
@@ -19,9 +19,9 @@ print(sock.recv(1024).decode())
 print(sock.recv(1024).decode())
 oper = input('multiply or add : ')
 sock.send(oper.encode())
-print(sock.recv(1024).decode())
+print(sock.recv(20).decode())
 
 #결과
 print(sock.recv(1024).decode())
-
+	
 sock.close()
--- week3/201802096_server.py
+++ week3/201802096_server.py
@@ -16,34 +16,36 @@ for i in range(3):
 	
 for i in clients:
 	i['conn'].send('Okay... All players have gathered. Start the game.'.encode())
-while True:
-	#난수 10개 생성
-	numbers = [ random.randrange(1,101) for _ in range(10) ]
-	scores = []
-	for i in clients:
-		#숫자 선택
-		i['conn'].send('Please select 1 number from 1 to 10.'.encode())
-		num = int(i['conn'].recv(1024).decode())
-		i['num1'] = numbers[num+1]
-		i['num2'] = random.randrange(-1, 5)
-		data = 'You chose the number ' + str(numbers[num+1]) + '. Please wait.'
-		i['conn'].send(data.encode())
-		
-	for i in clients:
-		#연산자 선택
-		i['conn'].send('Do you want multiply or add...?'.encode())
-		oper = i['conn'].recv(1024).decode()
-		if oper == 'add':
-			i['score'] = i['num1']+i['num2']
-		elif oper == 'multiply':
-			i['score'] = i['num1']*i['num2']
-		scores.append(i['score'])
-		i['conn'].send('Okay... please wait.'.encode())
-	scores.sort()
-		
-	for i in clients:
-		if i['score'] == score[0]:
-			i['conn'].send('Congratulations. You won!'.encode())
-		else:
-			i['conn'].send('Unfortunately, you hav been defeated.'.encode())
+
+#난수 10개 생성
+numbers = [ random.randrange(1,101) for _ in range(10) ]
+scores = []
+for i in clients:
+	#숫자 선택
+	i['conn'].send('Please select 1 number from 1 to 10.'.encode())
+	num = int(i['conn'].recv(1024).decode())
+	i['num1'] = numbers[num-1]
+	i['num2'] = random.randrange(-1, 5)
+	data = 'You chose the number ' + str(numbers[num-1]) + '. Please wait.'
+	i['conn'].send(data.encode())
+	
+for i in clients:
+	#연산자 선택
+	i['conn'].send('Do you want multiply or add...?'.encode())
+	oper = i['conn'].recv(1024).decode()
+	#점수 계산
+	if oper == 'add':
+		i['score'] = i['num1']+i['num2']
+	elif oper == 'multiply':
+		i['score'] = i['num1']*i['num2']
+	scores.append(i['score'])
+	i['conn'].send('Okay... please wait.'.encode())
+scores.sort()
+
+for i in clients:
+	#가장 높은 점수를 가진 client에게 승리 메시지를 보낸다.
+	if i['score'] == scores[2]:
+		i['conn'].send('Congratuations. You won!'.encode())
+	else:
+		i['conn'].send('Unfortunately, you hav been defeated.'.encode())
 conn.close()
