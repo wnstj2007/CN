54ac1824389e2ecebf0c39a75b8a4292d11d42fd
wnstj2007 <wnstj2096@gmail.com>
2020-10-07 10:00:17 +0900

    recv 추가

--- week4/201802096_locustfile.py
+++ week4/201802096_locustfile.py
@@ -6,10 +6,11 @@ class MyUser(User):
     @task
     def test(self):
         wait_time = between(1, 2)
-        
+
         ip = '192.168.1.10'
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((ip, 8888))
         data = 'GET / HTTP /1.1'
         client.send(data.encode)
+        data = client.recv(1024)
         client.close()
