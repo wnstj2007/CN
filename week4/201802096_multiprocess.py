4dc3b0c10fad45441484eaf54f77b3e03ce3fd5b
wnstj2007 <201802096@o.cnu.ac.kr>
2020-10-07 11:39:36 -0700

    과제 완성
    multi process 수정
    multi thread 수정

--- week4/201802096_multiprocess.py
+++ week4/201802096_multiprocess.py
@@ -3,20 +3,28 @@ import socket
 import os
 
 def send_recv(client):
-    pass
+	data = client.recv(1024)
+	print("[client {}] {}".format(os.getpid(), data.decode()))
+	data = "HTTP/1.1 200 OK\r\n"
+	client.send(data.encode())
+	client.close()
 
 def main(port):
-    proc = None
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('', port))
-    serversocket.listen(5)
-    client = []
+	proc = None
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind(('', port))
+	serversocket.listen(5)
+	client = []
 
-    try:
-        pass
-    except Exception:
-        proc.join()
+	try:
+		while True:
+			cli, address = serversocket.accept()
+			print("accept client from", address)
+			proc = Process(target = send_recv, args = (cli,))
+			proc.start()
+	except Exception:
+		proc.join()
 
 if __name__ == '__main__':
-    port = 8888
-    main(port)
+	port = 8888
+	main(port)
--- week4/201802096_multithread.py
+++ week4/201802096_multithread.py
@@ -2,22 +2,30 @@ from threading import Thread
 import socket
 import os
 
-def send_recv(client_socket):
-    pass
+def send_recv(client):
+	data = client.recv(1024)
+	print("[client {}] {}".format(os.getpid(), data.decode()))
+	data = "HTTP/1.1 200 OK\r\n"
+	client.send(data.encode())
+	client.close()
 
 def main(port):
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('', port))
-    th = None
-    print('listening...')
-    serversocket.listen(5)
-    client = []
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind(('', port))
+	th = None
+	print('listening...')
+	serversocket.listen(5)
+	client = []
 
-    try:
-        pass
-    except Exception:
-        th.join()
+	try:
+		while True:
+			cli, address = serversocket.accept()
+			print("accept client from", address)
+			th = Thread(target = send_recv, args = (cli,))
+			th.start()
+	except Exception:
+		th.join()
 
 if __name__ == '__main__':
-    port = 8888
-    main(port)
+	port = 8888
+	main(port)
