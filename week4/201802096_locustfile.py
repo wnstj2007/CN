076dfeda9b012f110bd02462563f52ec4d8150be
wnstj2007 <wnstj2096@gmail.com>
2020-10-07 09:55:15 +0900

    wait 추가

--- week4/201802096_locustfile.py
+++ week4/201802096_locustfile.py
@@ -1,10 +1,12 @@
 import time
 import socket
-from locust import User, task
+from locust import User, task, between
 
 class MyUser(User):
     @task
     def test(self):
+        wait_time = between(1, 2)
+        
         ip = '192.168.1.10'
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((ip, 8888))
